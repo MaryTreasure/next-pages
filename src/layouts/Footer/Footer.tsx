9c1b7da167b603d9cbf170193b09c59f58e01ad9
MaryTreasure <mari1991918@gmail.com>
2024-02-13 22:30:33 +0300

    add footer styles

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react'
 import { IFooterProps } from './Footer.props'
+import styles from './Footer.module.scss'
+import clsx from 'clsx';
+import {format} from 'date-fns';
+import Link from 'next/link';
 
-const Footer: FC<IFooterProps> = ({...props}) => {
+
+const Footer: FC<IFooterProps> = ({className, ...props}) => {
   return (
-    <div {...props}>Footer</div>
+    <footer {...props} className={clsx(className, styles.Footer)}>
+      <p>OwlTop © {format(new Date(), 'yyyy')} Все права защищены</p>
+      <Link className={styles.Footer__link} href={"/"} target='_blank'>Пользовательское соглашение</Link>
+      <Link className={styles.Footer__link} href={"/"} target='_blank'>Политика конфиденциальности</Link>
+    </footer>
   )
 }
 
--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,24 +1,21 @@
-import React, { FC } from 'react'
-import { ILayoutProps } from './Layout.props'
-import Header from '../Header/Header'
-import Sidebar from '../Siderbar/Sidebar'
-import Footer from '../Footer/Footer'
+import React, { FC } from "react";
+import { ILayoutProps } from "./Layout.props";
+import Header from "../Header/Header";
+import Sidebar from "../Siderbar/Sidebar";
+import Footer from "../Footer/Footer";
+import styles from './Layout.module.scss'
 
-const Layout: FC<ILayoutProps> = ({children}): JSX.Element => {
+const Layout: FC<ILayoutProps> = ({ children }): JSX.Element => {
   return (
-    <>
-    <Header />
-    <div>
-        <Sidebar  />
-        <div>
-            {children}
-        </div>
+    <div className={styles.Layout}>
+      <Header className={styles.Layout__header}/>
 
+      <Sidebar className={styles.Layout__sidebar}/>
+      <div className={styles.Layout__body}>{children}</div>
+
+      <Footer className={styles.Layout__footer}/>
     </div>
-    <Footer />
-    </>
-  )
-}
+  );
+};
 
 export default Layout;
-
